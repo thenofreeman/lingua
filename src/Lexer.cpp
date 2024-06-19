c3ccb04f351dbaea030f4c5b0ba136ca172e9308
nolan freeman abbott <nolan@freemanabbott.com>
2024-06-18 21:26:37 -0500

    fix logic for terminating lexer

--- src/Lexer.cpp
+++ src/Lexer.cpp
@@ -23,28 +23,30 @@ Token Lexer::readToken()
         readTokensFromLine();
     }
 
-    if (!tokens.empty())
-    {
-        Token outToken = tokens.at(popIndex++);
+    Token outToken = tokens.at(popIndex++);
 
-        if (popIndex >= tokens.size())
-            tokens.erase(tokens.begin(), tokens.end());
+    if (popIndex >= tokens.size())
+        tokens.erase(tokens.begin(), tokens.end());
 
-        return outToken;
-    }
-
-    return { TokenType::End, currentLine.value, currentLine.number, currentLine.position };
+    return outToken;
 }
 
 void Lexer::readTokensFromLine()
 {
+    if (inputstream.get() == EOF)
+    {
+        tokens.push_back({ TokenType::End, currentLine.value, currentLine.number, currentLine.position });
+        return;
+    }
+
     std::getline(inputstream, currentLine.value);
-std::cout << "LINE: " << currentLine.value << std::endl;
+    std::cout << "\nLINE: " << currentLine.value << std::endl;
 
     if (currentLine.value != "")
     {
         currentLine.number++;
         tokens = matcher.matchLine(currentLine);
-        tokens.push_back({ TokenType::Whitespace, "\\n", currentLine.number, currentLine.value.length() });
     }
+
+    tokens.push_back({ TokenType::Whitespace, "\\n", currentLine.number, currentLine.value.length() });
 }
